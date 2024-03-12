50d0d45895f6e5db2c372c0018aa00c7201342ea
Msolis314 <solisfanny2021@gmail.com>
2024-03-12 15:20:11 -0600

    Actualizando el README y el holaMundo
    Signed-off-by: Msolis314 <solisfanny2021@gmail.com>

--- holaMundo.cpp
+++ holaMundo.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 
 int main() {
-    std:: cout << "Hello World!";
+    std:: cout << "Hello World!\n";
+    std:: cout << "Hola Mundo a todos y todas, estudiantes de IE0217!\n";
+    std:: cout << "Probando el stash!";
     return 0;
 }
