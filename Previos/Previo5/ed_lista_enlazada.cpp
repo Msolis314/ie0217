5263f5c098a01740ba9b5e7914d45fde6a68871d
Msolis314 <solisfanny2021@gmail.com>
2024-04-02 21:02:06 -0600

    Se agregan los ejemplos de estructuras
    Signed-off-by: Msolis314 <solisfanny2021@gmail.com>

---
+++ Previos/Previo5/ed_lista_enlazada.cpp
@@ -0,0 +1,42 @@
+//Linked list implementation in C++
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+//Creating a node
+class Node{
+    public:
+        int value;
+        Node* next;
+};
+
+int main(){
+    Node* head;
+    Node* one = NULL;
+    Node* two = NULL;
+    Node* three = NULL;
+
+    //allocate 3 nodes in the heap
+    one = new Node(); // Declara un puntero a un objeto de tipo Node
+    two = new Node();
+    three = new Node();
+
+    //Assign value values
+    one->value = 1;
+    two->value = 2;
+    three->value = 3;
+
+    //Connect nodes
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    //print the linked list value
+    head = one;
+    while (head != NULL){
+        cout << head-> value;
+        head = head->next;
+    }
+    return 0;
+}
