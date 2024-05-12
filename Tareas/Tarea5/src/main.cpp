293697caf8d61c306e1915dbac41d6b62f77807f
Msolis314 <solisfanny2021@gmail.com>
2024-05-12 00:48:11 -0600

    (Tarea5) Se agrega una version preliminar del makefile y de los regex, faltan pulir muchos bichos
    Signed-off-by: Msolis314 <solisfanny2021@gmail.com>

--- Tareas/Tarea5/src/main.cpp
+++ Tareas/Tarea5/src/main.cpp
@@ -14,12 +14,13 @@ int main(){
         std::cout << "1) Ingresar email\n2) Salir" << std::endl;
         std::cin >> opcion;
         ValidadorEmail validador; 
+        bool valido = false;
         switch (opcion)
         {
             case INGRESAR_EMAIL:
                 std::cout << "Ingrese su email: ";
                 std::cin >> email;
-                validador.validarCorreo(email); 
+                validador.validarCorreo(email,valido); 
                 break;
             case SALIR:
                 std::cout << "Saliendo..." << std::endl;
